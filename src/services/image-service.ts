d4a01636074c94661797dbdf8934d5a13f9636ab
Erik Sundkvist <egsundkvist@hotmail.com>
2025-01-21 12:08:56 +0100

    hardcoded urls

--- src/services/image-service.ts
+++ src/services/image-service.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { getToken } from '../utils/localstorage'; // Importera getToken
 import { ImageData } from '../types/image'; // Importera typen ImageData
 
-console.log('VITE_IMAGE_API_URL:', import.meta.env.VITE_IMAGE_API_URL);
-const API_URL = import.meta.env.VITE_IMAGE_API_URL || 'http://localhost:8080';
+/* console.log('VITE_IMAGE_API_URL:', import.meta.env.VITE_IMAGE_API_URL);
+const API_URL = import.meta.env.VITE_IMAGE_API_URL; */
 
 const axiosInstance = axios.create({
-    baseURL: API_URL,
+    baseURL: "http://eriksdemoapi.chickenkiller.com",
     headers: {
         'Content-Type': 'application/json',
     },
--- src/services/user-profile-service.ts
+++ src/services/user-profile-service.ts
@@ -4,11 +4,11 @@ import { User } from '../types/user'; // Importera typen User
 import { Profile } from '../types/profile'; // Importera typen Profile
 
 
-console.log('VITE_USER_API_URL:', import.meta.env.VITE_USER_API_URL);
-const API_URL = import.meta.env.VITE_USER_API_URL || 'http://127.0.0.1:5000';
+/* console.log('VITE_USER_API_URL:', import.meta.env.VITE_USER_API_URL);
+const API_URL = import.meta.env.VITE_USER_API_URL; */
 
 const axiosInstance = axios.create({
-    baseURL: API_URL,
+    baseURL: "http://userapi.chickenkiller.com",
     headers: {
         'Content-Type': 'application/json',
     },
